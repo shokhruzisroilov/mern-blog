82270b3fafb2429d7c28427bfc763092ebc15b7e
Shohruz <shokhruzisroilov@gmail.com>
2024-10-04 19:46:54 +0500

    add admin factionality todo user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,7 +45,11 @@ export const signin = async (req, res, next) => {
 			return next(errorHandler(400, 'Invalid password'))
 		}
 		const token = jwt.sign(
-			{ id: validUser._id, username: validUser.username },
+			{
+				id: validUser._id,
+				username: validUser.username,
+				isAdmin: validUser.isAdmin,
+			},
 			process.env.JWT_SECRET
 		)
 		const { password: pass, ...rest } = validUser._doc
@@ -66,7 +70,10 @@ export const google = async (req, res, next) => {
 	try {
 		const user = await User.findOne({ email })
 		if (user) {
-			const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+			const token = jwt.sign(
+				{ id: user._id, isAdmin: user.isAdmin },
+				process.env.JWT_SECRET
+			)
 			const { password, ...rest } = user._doc
 			res
 				.status(200)
@@ -86,7 +93,10 @@ export const google = async (req, res, next) => {
 				profilePicture: googlePhotoUrl,
 			})
 			await newUser.save()
-			const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+			const token = jwt.sign(
+				{ id: user._id, isAdmin: newUser.isAdmin },
+				process.env.JWT_SECRET
+			)
 			const { password, ...rest } = user._doc
 			res
 				.status(200)
